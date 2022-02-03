201230e14e83cdd9016281de83e80ead6ddeffee
mikhaelle <buenomikhaelled@johndeere.com>
2022-02-03 11:54:21 -0300

    config tab navigation

--- MinaApp/src/components/BottomTab.tsx
+++ MinaApp/src/components/BottomTab.tsx
@@ -1,10 +1,16 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RouteProp, ParamListBase } from '@react-navigation/native';
 import React from 'react';
 import { AboutScene } from '../scenes/AboutScene/AboutScene';
 import { HomeScene } from '../scenes/HomeScene/HomeScene';
 import { ProfileScene } from '../scenes/ProfileScene/ProfileScene';
 import { SymptomScene } from '../scenes/SymptomScene/SymptomScene';
 import { TaskScene } from '../scenes/TaskScene/TaskScene';
+import Icon from 'react-native-vector-icons/AntDesign';
+import { View } from 'react-native';
+import { styles } from './BottomTab.css';
+import PostScreen  from './NewCicle';
+import LinearGradient from 'react-native-linear-gradient';
 
 
 export const ProfileComponent = () => <ProfileScene />;
@@ -16,14 +22,59 @@ export const HomeComponent = () => <HomeScene />;
 
 const Tab = createBottomTabNavigator();
 
-export default function Tabnavigator() {
+const screenOptions = (route: RouteProp<ParamListBase, string>) => {
+  let iconName;
+
+  switch (route.name) {
+    case 'Home':
+      iconName = 'home';
+      break;
+    case 'Profile':
+      iconName = 'user';
+      break;
+    case 'Task':
+      iconName = 'form';
+      break;
+      case 'Symptoms':
+        iconName = 'hearto';
+        break;
+    default:
+      break;
+  }
+
+  return <Icon name={iconName}  size={24} />;
+};
+
+const Tabnavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator 
+    screenOptions={({route}) => ({
+      tabBarIcon: () => screenOptions(route),
+    })}
+    tabBarOptions={{
+      showLabel: false,
+    }}>
       <Tab.Screen name="Home" component={HomeComponent} />
       <Tab.Screen name="Profile" component={ProfileComponent} />
-      <Tab.Screen name="Tasks" component={TaskComponent} />
-      <Tab.Screen name="Symptoms" component={SymptomsComponent} />
-      <Tab.Screen name="About" component={AboutComponent} />
+      <Tab.Screen
+      name="Post"
+      component={PostScreen}
+      options={() => ({
+        tabBarIcon: ({}) => (
+        <View>
+          <LinearGradient style={styles.iconTabRound} start={{ x: 0, y: 1 }} end={{ x: 0, y: 0 }} colors={['#EB5757','#FFD4D5']}>
+					<Icon name="plus" size={26} color='#FFF'/>
+				</LinearGradient>
+			</View>
+		),
+	})}
+/>
+      <Tab.Screen name="Task" component={TaskComponent} />
+      <Tab.Screen name="Symptoms" component={TaskComponent} />
+
+
     </Tab.Navigator>
   );
 }
+
+export default Tabnavigator;
