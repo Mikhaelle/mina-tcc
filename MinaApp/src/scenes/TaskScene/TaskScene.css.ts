00a83fc49df310b1300227f2dd70cac85000f4dc
mikhaelle <buenomikhaelled@johndeere.com>
2022-03-14 00:19:22 -0300

    add task prevision scene dynamicaly with the db

--- MinaApp/src/scenes/TaskScene/TaskScene.css.ts
+++ MinaApp/src/scenes/TaskScene/TaskScene.css.ts
@@ -1,18 +1,52 @@
 import styled from 'styled-components/native';
 import * as theme from '../../assets/variables.css';
+import {Dimensions} from 'react-native';
 
-export const FormText = styled.Text`
+export const BoxText = styled.Text`
   color: ${theme.BLACK};
   font-size: ${theme.FONT_SIZE_CUSTOM_MEDIUM};
   line-height: 16px;
+  font-weight: 400;
+`;
+
+export const TitleText = styled.Text`
+  color: ${theme.BLACK};
+  font-size: ${theme.FONT_SIZE_CUSTOM_MEDIUM};
+  line-height: 16px;
+  font-weight: 700;
+  padding: 20px;
+`;
+
+export const BoxView = styled.View`
+margin-top:10px
+background-color: #FDF1F1;
+border-color: #FECACA;
+border-width:1px;
+width:100%
+align-items: center;
+padding: 20px;
+font-family: ${theme.FONT_FAMILY};
+`;
+
+export const LineBoxView = styled.View`
+  background-color: #fdf1f1;
+  border-color: #fecaca;
+  border-width: 1px;
+  width: ${Dimensions.get('window').width + 'px'};
 `;
 
 export const View = styled.View`
   background-color: ${theme.WHITE};
   height: 100%;
   align-items: center;
-  padding: 25px;
-  padding-top: 80px;
-  padding-bottom: 80px;
   font-family: ${theme.FONT_FAMILY};
 `;
+
+export const TaskView = styled.View`
+  width: ${Dimensions.get('window').width - 20 + 'px'};
+  margin:10px;
+flex-direction:row
+justify-content:space-between;
+`;
+
+export const TaskButton = styled.TouchableOpacity``;
--- MinaApp/src/scenes/TaskScene/TaskScene.tsx
+++ MinaApp/src/scenes/TaskScene/TaskScene.tsx
@@ -1,13 +1,75 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {FormText, View} from './TaskScene.css';
+import React, {useEffect, useState} from 'react';
+import {Image} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useTask} from '../../contexts/TaskContext/TaskContext';
+import {
+  BoxText,
+  BoxView,
+  LineBoxView,
+  TaskButton,
+  TaskView,
+  TitleText,
+  View,
+} from './TaskScene.css';
+import images from '../../managers/images'
 
 export const TaskScene: React.FC = () => {
   const navigation = useNavigation();
 
+  const {getUserTasks, userTasks} = useTask();
+  const [userTasksComponents, setUserTasksComponents] = useState([] as any);
+
+  useEffect(() => {
+    getUserTasks();
+  }, []);
+
+  useEffect(() => {
+    if (userTasks.length !== 0) {
+      taskList();
+    }
+  }, [userTasks]);
+
+  const taskList = () => {
+    Object.keys(userTasks).forEach(userTask => {
+      const userTaskObj = userTasks[userTask];
+      const newTaskComponent = (
+        <>
+        <TaskView>
+          <TaskButton>
+            <Image source={images[userTask]} />
+          </TaskButton>
+          <TitleText>{userTaskObj['taskName']}</TitleText>
+          <Image source={images[userTaskObj['taskPrediction']]} /></TaskView>
+        </>
+      );
+
+      setUserTasksComponents((userTasksComponent: any) => [
+        ...userTasksComponent,
+        newTaskComponent,
+      ]);
+      userTasksComponents.push();
+    });
+  };
+
   return (
-    <View>
-      <FormText>About</FormText>
-    </View>
+    <>
+      <View>
+        <BoxView>
+          <BoxText>
+            Ajude-nos a te dar uma previsão melhor. Selecione o icone das previsões de
+            tarefas corrretas
+          </BoxText>
+        </BoxView>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <TitleText>Tarefas</TitleText>
+          <LineBoxView />
+          
+            {userTasksComponents.map(
+              (userTasksComponent: any) => userTasksComponent,
+            )}
+        </ScrollView>
+      </View>
+    </>
   );
 };
